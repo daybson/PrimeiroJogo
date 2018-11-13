7952c375bd5e8726796e29f5d1b205c03fb8e453
Daybson Paisante <daybson.paisante@outlook.com>
2018-11-13 09:28:36 -0200

    + scrpts de atirar e mover o projétil

---
+++ Assets/Scripts/Personagem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    public Transform transformMira;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projetil = (GameObject)Instantiate(Resources.Load("Prefabs/Projetil"));
+            projetil.transform.position = this.transformMira.position;
+            projetil.transform.forward = this.transformMira.forward;
+        }
+    }
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+    public float Velocidade;
+
+    void Update()
+    {
+        transform.Translate(transform.forward * this.Velocidade * Time.deltaTime, Space.World);
+    }
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
