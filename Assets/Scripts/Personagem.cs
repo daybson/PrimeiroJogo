5ec363693dfc2fd582ad617613db0bc44a220a0d
Daybson Paisante <daybson.paisante@outlook.com>
2018-11-13 12:38:55 -0200

    + adicionando pulo

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class Personagem : MonoBehaviour
 {
     public Transform transformMira;
+    public Rigidbody rigidbody;
+    public float VelocidadePulo;
+    public int pulosMaximos;
+    public int pulosContador;
 
+
+    private void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Realiza o pulo
+        if (this.pulosContador < this.pulosMaximos && 
+            Input.GetKeyDown(KeyCode.Space))
+        {
+            this.pulosContador++;
+            this.rigidbody.AddForce(Vector3.up * this.VelocidadePulo, ForceMode.Impulse);
+        }
+
+        //Dispara Projétil
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject projetil = (GameObject)Instantiate(Resources.Load("Prefabs/Projetil"));
             projetil.transform.position = this.transformMira.position;
             projetil.transform.forward = this.transformMira.forward;
+        }
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag.Equals("plataforma"))
+        {
+            this.pulosContador = 0;
         }
     }
 }
