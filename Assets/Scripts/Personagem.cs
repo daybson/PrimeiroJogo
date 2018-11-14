083b0d178d8991540edd7a6b3610ffa758bfabe0
Daybson Paisante <daybson.paisante@outlook.com>
2018-11-14 11:00:23 -0200

    * prefab Personagem com novos scripts + rotação pelo mouse + movimentação

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -10,8 +10,42 @@ public class Personagem : MonoBehaviour
     public int PulosMaximos;
     public int PuloContador;
 
+    public float VelocidadeRotacao;
+
+    public float VelocidadeMovimento;
+
     void Update()
     {
+        //Obtem o movimento horizontal do mouse
+        var orientacaoY = Input.GetAxis("Mouse X");
+        if (orientacaoY != 0)
+        {
+            //Calcula a quantidade de rotação/s gerada pelo movimento do mouse
+            var rotacaoDelta = Quaternion.Euler(0, orientacaoY * this.VelocidadeRotacao * Time.deltaTime, 0);
+
+            //Aplica a rotação ao rigidbody multiplicando a rotação atual pela delta, ou seja, combinando as duas
+            this.rigidbody.MoveRotation(this.rigidbody.rotation * rotacaoDelta);
+        }
+
+
+        //Obtem entrada do usuário com teclado nos eixos vertical e horizontal
+        var entradaEixosMovimento = new Vector3(Input.GetAxis("Horizontal"),
+                                                0,
+                                                Input.GetAxis("Vertical"));
+
+        //Se usuário pressionou algum eixo
+        if(entradaEixosMovimento != Vector3.zero)
+        {
+            //Converte a entrada do eixo de movimento do espaço global para espaço local do personagem
+            entradaEixosMovimento = transform.TransformDirection(entradaEixosMovimento);
+
+            //Escalona o eixo de input do usuário pela velocidade/s
+            var movimentoDelta = entradaEixosMovimento * this.VelocidadeMovimento * Time.deltaTime;
+
+            //Soma o deslocamento gerado com a posição atual, gerando a nova posição
+            this.rigidbody.MovePosition(this.rigidbody.position + movimentoDelta);
+        }
+
         //Realiza o pulo
         if (this.PuloContador < this.PulosMaximos && Input.GetKeyDown(KeyCode.Space))
         {
